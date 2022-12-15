5168b5619f7e213c823822641373be1ea555bd17
bhavanigudla <120613454+bhavanigudla@users.noreply.github.com>
2022-12-14 20:21:56 -0600

    new one

---
+++ NEWTestNG/src/test/java/NewTestNG/NewTest.java
@@ -0,0 +1,63 @@
+package NewTestNG;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class NewTest {
+  @Test
+  public void testCaseOne() {
+	  System.out.println("TEST CASE 1 ");
+  }
+  @Test
+  public void testCaseTwo() {
+	  System.out.println("TEST CASE 2 ");
+  }
+  
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("before method");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("After method");
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("before Class");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("After Class");
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("before test");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("After Test");
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+	  System.out.println("before suite");
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+	  System.out.println("After suit");
+  }
+
+}
